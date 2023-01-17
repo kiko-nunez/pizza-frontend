c3120614e7b0ff3beed695ff94e95b49a3c67402
ayanahmed92 <ayanaaa1992@gmail.com>
2023-01-17 12:56:27 -0600

    updated App.js and added pages folder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route, Routes, BrowserRouter as Router} from "react-router-dom"
+import Main from './pages/Main'
+import Login from './pages/login';
+import Checkout from './pages/Checkout';
+import Cart from './pages/Cart';
+import Shop from './pages/Shop'
+import Nav from './components/Nav'
+
+
+
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Router>
+    <Routes>
+      <Route exact path="/" element={<Main />} />
+      <Route path= "/Cart" element={<Cart />} />
+      <Route path= "Checkout" element={<Checkout />} />
+      <Route path= "Login" element={<Login />} />
+      <Route path= "Shop" element={<Shop />} />
+    </Routes>
+  </Router>
     </div>
   );
 }
