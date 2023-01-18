865af9a3552a57146e7cbd03a7af4e2dd1ae001b
Firstname Lastname <githubemail@domain.com>
2023-01-18 14:48:12 -0700

    creates form for checkout page

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from 'react';
 
-const Checkout = () => {
+function Checkout(props) {
   return (
-    <div>Checkout</div>
-  )
+    <div className="checkout">
+      <h1>Checkout</h1>
+      <form>
+        <label>
+          Cardholder's Name 
+          <input
+            type="text"
+            name="name" 
+         />
+        </label>
+        <label>
+          Card Number 
+          <input
+            type="number"
+            name="cardNumber" 
+         />
+        </label>
+        <label>
+          Expiration Date  
+          <input
+            type="number"
+            name="expDate" 
+         />
+        </label>
+        <label>
+          CCV 
+          <input
+            type="number"
+            name="cvv" 
+         />
+        </label>
+      
+      </form>
+
+      <button onClick={alert}> Place Order
+      </button>
+    </div>
+  );
 }
 
 export default Checkout
