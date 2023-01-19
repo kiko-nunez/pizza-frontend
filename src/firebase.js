089df795f270c30cb8bc2a1d4d6705385c9605c6
Firstname Lastname <githubemail@domain.com>
2023-01-19 10:02:56 -0700

    storing changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,48 +1,32 @@
-<<<<<<< HEAD
-// import { initializeApp } from  'firebase/app'
-// import { GoogleAuthProvider, signInWithPopup, getAuth, signOut } from 'firebase/auth'
-// import { logIn, logOut } from '../firebase'
-// import { Link } from 'react-router-dom'
-=======
 
 /*import { initializeApp } from  'firebase/app'
 
 import { GoogleAuthProvider, signInWithPopup, getAuth, signOut } from 'firebase/auth'
 import { logIn, logOut } from '../firebase'
 import { Link } from 'react-router-dom'
->>>>>>> 7974f5a5a9ae7e09e2996ca9f2159d378f08fd0d
 
 
 
-// //App's Firebase configuration 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCYNLnNFTtNjbgYJMoiO93vudxSKLdcmE4",
-//     authDomain: "cheezus-995bb.firebaseapp.com",
-//     projectId: "cheezus-995bb",
-//     storageBucket: "cheezus-995bb.appspot.com",
-//     messagingSenderId: "397414007524",
-//     appId: "1:397414007524:web:034bf6b6fe759d3febe2f1",
-//     measurementId: "G-CMBT8MDEXB"
-// }
+//App's Firebase configuration 
+const firebaseConfig = {
+    apiKey: "AIzaSyCYNLnNFTtNjbgYJMoiO93vudxSKLdcmE4",
+    authDomain: "cheezus-995bb.firebaseapp.com",
+    projectId: "cheezus-995bb",
+    storageBucket: "cheezus-995bb.appspot.com",
+    messagingSenderId: "397414007524",
+    appId: "1:397414007524:web:034bf6b6fe759d3febe2f1",
+    measurementId: "G-CMBT8MDEXB"
+}
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-// // Instanstiate Providers
-// const provider = new GoogleAuthProvider()
+// Instanstiate Providers
+const provider = new GoogleAuthProvider()
 
-// // Get current auth instance
-// export const auth = getAuth(app)
+// Get current auth instance
+export const auth = getAuth(app)
 
-<<<<<<< HEAD
-// // setting up authentication function
-// export function logIn () {
-//     return signInWithPopup(auth, provider)
-// }
-// export function logOut () {
-//     return signOut(auth)
-// }
-=======
 // setting up authentication function
 export function logIn () {
     return signInWithPopup(auth, provider)
@@ -51,4 +35,3 @@ export function logOut () {
     return signOut(auth)
 } */
 
->>>>>>> 7974f5a5a9ae7e09e2996ca9f2159d378f08fd0d
