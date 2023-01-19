91ac622b546ab3f6fa63d8914798a9b370300890
kiko-nunez <117489647+kiko-nunez@users.noreply.github.com>
2023-01-18 21:33:56 -0700

    adds functions for handle submit and handle change

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,6 +1,30 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Register = (props) => {
+  const formFields = {
+    name: "",
+    address: "",
+    phone: "",
+  }
+
+
+  const [newUser, setNewUser] = useState(formFields)
+
+  function handleSubmit (e) {
+    e.preventDefault()
+    props.createPeople(newUser)
+    setNewUser(formFields)
+  }
+
+  function handleChange (e) {
+    setNewUser((prevState) =>  ({
+      ...prevState,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+
   return (
 <div>
 <h1>Register User</h1>
@@ -8,26 +32,28 @@ const Register = (props) => {
 Name: <input
 type="text"
 name="name"
-value=""
+onChange={handleChange}
+value={newUser.name}
 placeholder='Mr. Cheezus'
 />
 Street Address: <input
 type="text"
 name="address"
-value=""
+onChange={handleChange}
+value={newUser.address}
 placeholder='123 Main St'
 />
 Phone Number: <input
 type="number"
 name="phone"
-value=""
+onChange={handleChange}
+value={newUser.phone}
 placeholder='1(800)-CHEEZUS'
 />
 </form>
 <input className='register' type="submit" value="Register User"/>
 
 </div>
-
   )
 }
 
