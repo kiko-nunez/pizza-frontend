82ce8f092bdbbc5521b5191db0db0299a0de0c48
krsnamara <martinj.fitzpatrick@gmail.com>
2023-01-19 00:51:48 -0500

    updating to start working in dev branch

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,22 +1,13 @@
 import React from 'react'
-import Navbar from '../components/Nav'
-import Images from "../images/Cheezus.png"
 
 const Main = () => {
   return (
-    <>
+    <div>
+      <h1>Login or Register</h1> 
+      <button onClick={logout}>Logout</button>
 
-    <div className="storeInfo">
-
-      <div className="store">  <img className="img"src={Images}/> Store 1</div>
-      <div className="store">Store 2</div>
-      <div className="store">Store 3</div>
-      <div className="store">Store 4</div>
-   </div>
-  
-    </>
-   
- )
+    </div>
+  )
 }
 
 export default Main
