178b7f2de5336fd1c89d634b3026715292033ba6
kiko-nunez <117489647+kiko-nunez@users.noreply.github.com>
2023-01-19 13:27:31 -0700

    makes fetch call to heroku url and displays shops

--- src/pages/Shops.js
+++ src/pages/Shops.js
@@ -1,30 +1,18 @@
 
-import React, { useState } from 'react'
+// import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 // import Navbar from '../components/Nav'
 
-const Shops = () => {
-  const [shops, setShops] = useState()
-  console.log(shops);
-
-  const URL = 'https://cheesus.herokuapp.com/'
-
-
-  const getShops = async () => {
-
-    const response = await fetch(URL, {
-      method: 'GET'
-    })
-    const data = await response.json()
-    setShops(data)
-  }
-  
+function Shops (props) {
+console.log(props);
   const loaded = () => {
-    return shops.map(shop => (
+    return props.shops.map(shop => (
       <div key={shop._id}>
-        <Link to={`/shops/${shop._id}`}>
-          <h1>Name: {shop.name} </h1>
-          <h2>{shop.image}</h2>
+        <Link to={`/cart`}>
+          <h1>Name: {shop.shop_name} </h1>
+          <img src={shop.img} alt={shop.shop_name}/>
+          <h2>{shop.phone}</h2>
+          <h2>{shop.address}</h2>
         </Link>
       </div>
     ))
@@ -36,13 +24,9 @@ const loading = () => {
 
   return (
     <section>
-    <div className="storeInfo">
-      {shops}
-   </div>
-   {shops ? loaded() : loading()}
+   {props.shops ? loaded() : loading()}
     </section>
    
- )
-  }
+ )}
 
 export default Shops
